cc4584d8508b17eef551faf7fd71366be49430ad
Raphael Boullay <raphael.boullaylefur@lyceeconnecte.fr>
2026-03-14 23:11:41 +0100

    Update KunManga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/KunManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/KunManga.kt
@@ -1,5 +1,6 @@
 package org.koitharu.kotatsu.parsers.site.madara.en
 
+import okhttp3.Headers
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
@@ -7,4 +8,17 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("KUNMANGA", "KunManga", "en")
 internal class KunManga(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.KUNMANGA, "kunmanga.com", 10)
+    MadaraParser(context, MangaParserSource.KUNMANGA, "kunmanga.com", 10) {
+
+    override val headers: Headers = Headers.Builder()
+        .add(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                "Chrome/120.0.0.0 Safari/537.36",
+        )
+        .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+        .add("Accept-Language", "en-US,en;q=0.5")
+        .add("Referer", "https://kunmanga.com/")
+        .build()
+}
